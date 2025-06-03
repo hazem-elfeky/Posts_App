d07d04907424f68101b48cf7258415fae0e2e304
hazem elfeky <hazem.fawzym19@gmail.com>
2025-06-03 22:14:22 +0300

    Update posts_repository.dart

--- posts/lib/features/posts/domain/repositories/posts_repository.dart
+++ posts/lib/features/posts/domain/repositories/posts_repository.dart
@@ -4,10 +4,10 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/error/failures.dart';
 
 abstract class PostsRepository {
-  //الانتر فيس بتاعي اللي فيه من خلاله جميع الفانكشن اللي ممكن اعملها ع البوستات
+
   Future<Either<Failure, List<Post>>> getAllPosts();
   Future<Either<Failure, Unit>> deletePost(
-      int id); //الفيلير ع الشمال عشان هي الغلط  والرايت يعني صح يعني يونيت اليونيت كانها فويد
+      int id); 
   Future<Either<Failure, Unit>> updatePost(Post post);
   Future<Either<Failure, Unit>> addPost(Post post);
 }
