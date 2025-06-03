48ebcb285998a1b1f132b0787efadf0512f3ff1a
hazem elfeky <hazem.fawzym19@gmail.com>
2025-06-03 21:54:29 +0300

    Delete posts/lib/core directory

--- posts/lib/core/error/exceptions.dart
+++
@@ -1,6 +0,0 @@
-class ServerException
-    implements Exception {} //الكلاسات الاكسيبشن اللي بتقابل الفيلير اللي عنيدي
-
-class EmptyCacheException implements Exception {}
-
-class OfflineException implements Exception {}
